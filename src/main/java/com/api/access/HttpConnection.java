90afe4f2aa5936ab1ddd9e2c645bff9e69e9db81
Sebastien Dugene <sebastien.dugene@gmail.com>
2017-06-02 15:44:56 +0200

    SO > api.access > 5th test

--- src/main/java/com/api/access/HttpConnection.java
+++ src/main/java/com/api/access/HttpConnection.java
@@ -29,15 +29,7 @@ public class HttpConnection {
         System.out.println(this.method);
         try {
             url = new URL(urlString.toString());
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod(this.method);
-            urlConnection.setDoOutput(true);
-            urlConnection.setDoInput(true);
-            urlConnection.setRequestProperty("Content-Type", "application/json");
-            urlConnection.setRequestProperty("auth", "3c725b82ec06903cc43c6fe0980c0e7d");
-            OutputStreamWriter wr = new OutputStreamWriter(urlConnection.getOutputStream());
-            wr.write(params);
-            urlConnection.connect();
+            urlConnection = get(url);
             inStream = urlConnection.getInputStream();
             BufferedReader bReader = new BufferedReader(new InputStreamReader(inStream));
             String temp, response = "";
@@ -64,5 +56,31 @@ public class HttpConnection {
 
 
         return "ok";
+    }
+
+    private HttpURLConnection get(URL url) throws Exception
+    {
+        urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("GET");
+        urlConnection.setDoOutput(true);
+        urlConnection.setDoInput(true);
+        urlConnection.setRequestProperty("Content-Type", "application/json");
+        urlConnection.setRequestProperty("auth", "3c725b82ec06903cc43c6fe0980c0e7d");
+        urlConnection.connect();
+        return urlConnection;
+    }
+
+    private HttpURLConnection post(URL url, String params) throws Exception
+    {
+        urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("POST");
+        urlConnection.setDoOutput(true);
+        urlConnection.setDoInput(true);
+        urlConnection.setRequestProperty("Content-Type", "application/json");
+        urlConnection.setRequestProperty("auth", "3c725b82ec06903cc43c6fe0980c0e7d");
+        OutputStreamWriter wr = new OutputStreamWriter(urlConnection.getOutputStream());
+        wr.write(params);
+        urlConnection.connect();
+        return urlConnection;
     }
 }
